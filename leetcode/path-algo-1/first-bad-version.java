4b2fb906d84e9405090d14c7538f951439131f4e
wasobi <obi.sierra@gmail.com>
2022-03-30 22:54:42 -0400

    Starting First Bad Version

---
+++ leetcode/path-algo-1/first-bad-version.java
@@ -0,0 +1,20 @@
+/*
+    Attempt 1
+
+    Source: https://leetcode.com/problems/first-bad-version/
+
+    Possible input
+    - n number of versions
+
+    Possible output
+    - the number of the first bad version (given through API)
+*/
+
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        
+    }
+}
