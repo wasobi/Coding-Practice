bbac495287133ea9f382201700f9826d4b196729
wasobi <obi.sierra@gmail.com>
2022-03-31 07:48:06 -0400

    Final solution for First Bad Version using binary search in the implementation

--- leetcode/path-algo-1/first-bad-version.java
+++ leetcode/path-algo-1/first-bad-version.java
@@ -3,14 +3,29 @@
 
     Source: https://leetcode.com/problems/first-bad-version/
 
-    Possible input
+    Possible input:
     - n number of versions
 
-    Possible output
+    Possible output:
     - the number of the first bad version (given through API)
 
-    Soulution
-    implement a binary search to find the first call
+    Assumptions:
+    - the output will always be a positive integer
+    - there will always be value of n that is an answer
+
+    Soulution: Implement a binary search to find the first bad version
+    search for the first bad version while your left bound is less than your right bound
+    - calculate the new pivot by finding the midpoint i.e. n/2
+    - if it is not a bad version then
+    --> reposition the left bound to be to the right of the pivot to check the versions of n > pivot
+    - if it is a bad version then
+    --> reposition the right bound to be to the left of the pivot to check the versions of n < pivot
+    - when the left bound crosses the right bound, you have checked every version and will exit the loop
+    - left will represent the bad version with the lowest value of n
+
+    Analysis:
+    Time complexity - O(log n) because the search was done on n sorted. In the worst case, we have to check log n elements to find the answer. 
+    Space complexity - O (1) because no data structures were used to store data so it takes constant time to acces an element.
 */
 
 /* The isBadVersion API is defined in the parent class VersionControl.
@@ -18,23 +33,20 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int pivot = n/2;
-        int startIndex = 0;
-        int endIndex = 0;
-        bool badVersion = false;
-
-        for (int i = 0; i < n; i++) {
+        int pivot;
+        int left = 1;
+        int right = n;
+        boolean badVersion = false;
+        while (left <= right) {
+            pivot = left + (right - left)/2;
             badVersion = isBadVersion(pivot);
-            if (badVersion == false) {
-                // search for values of n > pivot
-                // reset the left
+            if (!badVersion) {
+                left = pivot + 1;
             }
-            else if (badVersion == true) {
-                // search for values of n < pivot
-                // reset the right
+            else if (badVersion) {
+                right = pivot - 1;
             }
-            // calculate the new pivot by finding the middle call i.e.
-            pivot = left + (right - left);
         }
+        return left;
     }
 }
