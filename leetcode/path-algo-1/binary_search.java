14f84a2451bd87bf58be1d193f612c4db3d7b8be
wasobi <obi.sierra@gmail.com>
2022-03-30 22:48:44 -0400

    finished Binary Search

--- leetcode/path-algo-1/binary_search.java
+++ leetcode/path-algo-1/binary_search.java
@@ -4,27 +4,39 @@
     Source: https://leetcode.com/problems/binary-search/
 
     Possible input
-    - empty array
-    - negative numbers
+    - array of integers sorted in ascending order
+    - all unique numbers
+
+    Possible output
+    - if the number exists, return the index
+    - if the number does not exist, return -1
+
+    ex.
+    [0,2,4,5,6,7,8,9]
+     - - - -|p - - -
+     - - - - - - |p -
 */
 
 class Solution {
     public int search(int[] nums, int target) {
-        len = nums.length;
-        pivot = nums[len/2];
-        if () {
-            if () {
+        int left = 0;
+        int right = nums.length - 1;
+        int pivot = (right - left)/2; // index of the middle most element in the subarray
 
+        while (left <= right) {
+            if (nums[pivot] == target) {
+                return pivot;
             }
-            else {
-
+            // if the pivot is greater than the target, search  the left subarray
+            if (nums[pivot] > target) {
+                right = pivot - 1;
             }
+            // if the pivot is smaller than the target, search  the right subarray
+            else if (nums[pivot] < target) {
+                left = pivot + 1;
+            }
+            pivot = left + (right - left)/2;
         }
-        if else () {
-            return
-        }
-        else {
-            return null;
-        }
+        return -1;
     }
 }
