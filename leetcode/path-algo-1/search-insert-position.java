ba876f6eea397f1219c32de43d9711fd13b29f8f
wasobi <obi.sierra@gmail.com>
2022-03-31 23:30:05 -0400

    Completed Search Insert Position -- two Attempts

--- leetcode/path-algo-1/search-insert-position.java
+++ leetcode/path-algo-1/search-insert-position.java
@@ -36,6 +36,53 @@
 
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        
+        int pivot;
+        int left = 0;
+        int right = nums.length/2;
+        boolean found = false;
+
+        while (left <= right) {
+            pivot = left + (right  - left)/2;
+            if (nums[pivot] == target) {
+                found = true;
+            }
+            else if (nums[pivot] > target) {
+                right = pivot - 1;
+            }
+            else {
+                left =  pivot + 1;
+            }
+        }
+        if (!found)
+        {
+            return left;
+        }
+        return pivot;
+    }
+}
+
+
+/*
+    Attempt 2
+*/
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+           int pivot = left + (right  - left)/2;
+            if (nums[pivot] == target) {
+                return pivot;
+            }
+            else if (nums[pivot] > target) {
+                right = pivot - 1;
+            }
+            else {
+                left =  pivot + 1;
+            }
+        }
+        return left;
     }
 }
