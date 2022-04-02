53f234cca5f030097940d8151858915e9179cc8a
wasobi <obi.sierra@gmail.com>
2022-04-01 21:03:22 -0400

    Recreating the problems from the mock interview to solve in a second attempt

---
+++ mock-interviews/distance-from-center.java
@@ -0,0 +1,35 @@
+/*
+    Attempt 1
+
+    Mock Interview from interview.io
+
+    Problem:
+    Given an array <rectangles> that contains the length and
+    width of n rectangles such that rectangle[n] = [length,width].
+    Find the largest square that can be made and return the number of times
+    the square of length = maxLen and width = maxWidth can be created.
+
+    Possible input:
+    - 2d array of integers
+    - empty array
+    - array of an infinite number of elements
+
+    Possible output:
+    - array of n = queries.length elements
+    - each index in the output array should represent how many points from the points array were located withing the circle
+
+    Assumptions:
+    - integers that represent coordinates on a plane can be positive or negative
+    - number of points has no correlation with the number of circles
+
+    Soulution:
+    - TODO
+
+    Analysis:
+    Time complexity - O ()
+    Space complexity - O ()
+*/
+
+/*
+*   DISTANCE EQUATION: sqrt((x1 - x2)^2 + (y1 - y2)^2)
+*/
---
+++ mock-interviews/largest-square.java
@@ -0,0 +1,30 @@
+/*
+    Attempt 1
+
+    Mock Interview from interview.io
+
+    Problem:
+    Given an array <rectangles> that contains the length and
+    width of n rectangles such that rectangle[n] = [length,width].
+    Find the largest square that can be made and return the number of times
+    the square of length = maxLen and width = maxWidth can be created.
+
+    Possible input:
+    - 2d array of integers
+    - empty array
+    - array of an infinite number of elements
+
+    Possible output:
+    - integer representing the number of times a square was created with the dimensions
+    of maxLen by maxWidth
+
+    Assumptions:
+    - the dimensions of each rectangle will always be a pair of positive integers
+
+    Soulution:
+    - TODO
+
+    Analysis:
+    Time complexity - O ()
+    Space complexity - O ()
+*/
