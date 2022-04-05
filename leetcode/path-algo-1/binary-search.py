f0ad92589fcfc502046844ac1d77a2d9c1259b6e
wasobi <obi.sierra@gmail.com>
2022-04-04 23:36:34 -0400

    Created Python files for the LeetCode Algorithms 1 path

---
+++ leetcode/path-algo-1/binary-search.py
@@ -0,0 +1,13 @@
+'''
+    Attempt 1
+
+    Source: https://leetcode.com/problems/binary-search/
+
+    Possible input
+    - array of integers sorted in ascending order
+    - all unique numbers
+
+    Possible output
+    - if the number exists, return the index
+    - if the number does not exist, return -1
+'''
