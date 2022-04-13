a76adecffb3698dbbc419be03a64e85e360ff7e9
wasobi <obi.sierra@gmail.com>
2022-04-13 11:33:52 -0400

    Planning solution for Attempt 2

--- mock-interviews/sales-path.java
+++ mock-interviews/sales-path.java
@@ -26,6 +26,35 @@
     Space complexity - O (1)
 */
 
+/*
+    Attempt 2
+
+    Source: https://www.pramp.com/challenge/15oxrQx6LjtQj9JK9XqA
+    Solution: https://www.pramp.com/question/15oxrQx6LjtQj9JK9XqA
+
+    Possible input:
+    - k-ary tree with n children
+    - empty tree
+    - k-ary tree with equivalent sales paths
+
+    Possible output:
+    - integer greater than 0
+
+    Assumptions:
+    - All nodes in a path will contain an integer between 0 and infinity
+
+    Soulution:
+    - traverse each path to the furthest child and calculate the total sales path
+    - the first sales path found with be the current minPath
+    --> if we find a sales path that is less than the minPath, update the minPath
+    - return the minPath
+
+    Analysis:
+    Time complexity - O (n^2)
+    Space complexity - O (1)
+*/
+
+
 import java.io.*;
 import java.util.*;
 
@@ -45,8 +74,8 @@ class Solution {
   }
 
   static class SalesPath {
-
-    int getCheapestCost(Node rootNode) {
+      // Attempt 1
+    int getCheapestCost1(Node rootNode) {
       // check each child of the root
       boolean found = false;
       int cheapestCost = 0;
@@ -63,6 +92,21 @@ class Solution {
         }
         // parent =
       }
+    }
+    // Attempt 2
+    int getCheapestCost2(Node rootNode) {
+      // check each child of the root
+      int minPath = 0;
+      Node currentNode = rootNode;
+
+      while (currentNode.children != null) {
+
+        if (current) {
+
+        }
+      }
+
+     return minPath;
     }
   }
 
